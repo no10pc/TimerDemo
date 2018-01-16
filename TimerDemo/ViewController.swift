0e69fc9520e3c2de5bb53efb687531ec116f43ec
no10pc <131501@qq.com>
2018-01-16 10:41:08 +0800

    first

--- TimerDemo/ViewController.swift
+++ TimerDemo/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var timer:Timer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @objc func tickDown()
+    {
+        print("tick...")
+    }
 
 }
 
